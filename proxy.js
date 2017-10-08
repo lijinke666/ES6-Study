fdd1d108f39814d355a6b2ca3c5ba97cbe261e2c
jinke.li <1359518268@qq.com>
2017-10-08 19:09:52 +0800

    add

--- proxy.js
+++ proxy.js
@@ -1,7 +1,11 @@
 const proxy = new Proxy({},{
    get(target,property){
    	return 666
-   }
+   },
+    set: function (target, key, value, receiver) {
+    console.log(`setting ${key}!`);
+    return Reflect.set(target, key, value, receiver);
+  }
 })
 
 proxy.time
